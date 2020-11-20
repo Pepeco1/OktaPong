27e258dc3cc59f35356e6a914a0218bcc93eb0b6
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-20 17:11:36 -0300

    Created projectile initial script

---
+++ OktaPong/Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MovableObjectMono
+{
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    protected override void Move()
+    {
+        Vector3 newPosition = transform.right * maxSpeed * Time.deltaTime;
+
+        transform.Translate(newPosition);
+    }
+}
