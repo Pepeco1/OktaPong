d0f0e9508e7b09f57cc37d9df80ccd469807f72a
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-20 14:36:20 -0300

    created classes to get input

---
+++ OktaPong/Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : InputProvider
+{
+    public override void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 moveValue = ctx.ReadValue<Vector2>();
+
+        horizontalInput = moveValue.x;
+        verticalInput = moveValue.y;
+    }
+
+    public override void OnShoot(InputAction.CallbackContext ctx)
+    {
+        shootInput = ctx.ReadValue<float>() > 0.5;
+    }
+
+}
