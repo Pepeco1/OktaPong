cd080c1a745fc84ac39e601eebec302327699011
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-20 19:19:29 -0300

    Made the ProjectileCollisionEffect class
    It is a easy way to create new and compose existing collision effects to projectiles

---
+++ OktaPong/Assets/Scripts/Projectile/ProjectileCollisionEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Projectile))]
+public abstract class ProjectileCollisionEffect : MonoBehaviour
+{
+
+    protected Projectile myProjectile = null;
+
+    private void Awake()
+    {
+        myProjectile = GetComponent<Projectile>();
+    }
+
+    private void OnEnable()
+    {
+        myProjectile.onCollide += Projectile_OnCollide;
+    }
+
+    private void OnDisable()
+    {
+        myProjectile.onCollide -= Projectile_OnCollide;
+    }
+
+    protected abstract void Projectile_OnCollide();
+}
