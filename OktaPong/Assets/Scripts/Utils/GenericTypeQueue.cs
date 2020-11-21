794718b2c665c4c38e514294dbb42e21f14673c2
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-21 12:02:54 -0300

    Created pool script and singleton script

---
+++ OktaPong/Assets/Scripts/Utils/GenericTypeQueue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTypeQueue<T> : MonoBehaviour where T : Component
+{
+
+    [SerializeField] private T prefab = null;
+
+    private Queue<T> inactiveObjects = new Queue<T>();
+
+    
+    public GenericTypeQueue(T prefab)
+    {
+        this.prefab = prefab;
+    }
+
+    public T GetInstance()
+    {
+
+        if (inactiveObjects.Count <= 0)
+            AddObjToQueue();
+
+        return inactiveObjects.Dequeue();
+        
+    }
+
+    public void ReturnToPool(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        inactiveObjects.Enqueue(obj);
+    }
+
+
+    private void AddObjToQueue()
+    {
+        var obj = Instantiate(prefab);
+        inactiveObjects.Enqueue(obj);
+    }
+
+}
