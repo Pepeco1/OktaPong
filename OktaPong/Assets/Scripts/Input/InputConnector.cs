1052c0cfb6eb5b6cab110a998d26ef68b493b803
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-22 14:03:04 -0300

    inputconnector working
    On awake it finds the input provider and copies the PlayerInput to same game object

---
+++ OktaPong/Assets/Scripts/Input/InputConnector.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class InputConnector : MonoBehaviour
+{
+    private void Awake()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+
+        var providers = FindObjectsOfType<PlayerInputProvider>().ToList();
+
+        // Finds the 
+        var provider = providers.Find(prov => prov.ID == playerInput.playerIndex);
+
+        // TODO - Copy playerInput component to the provider's GO;
+        //ComponentUtils.CopyComponent(playerInput, provider.gameObject);
+        provider.gameObject.AddComponent<PlayerInput>();
+        var newComponent  = provider.gameObject.GetComponent<PlayerInput>();
+        ComponentUtils.GetCopyOf(newComponent, playerInput);
+       
+    }
+
+}
---
+++ OktaPong/Assets/Scripts/Utils/ComponentUtils.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System;
+using UnityEngine;
+
+public static class ComponentUtils
+{
+    public static Component CopyComponent(Component original, GameObject destination)
+    {
+        System.Type type = original.GetType();
+        Component copy = destination.AddComponent(type);
+        // Copied fields can be restricted with BindingFlags
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(copy, field.GetValue(original));
+        }
+        return copy;
+    }
+
+    public static T GetCopyOf<T>(Component comp, T other) where T : Component
+    {
+        Type type = comp.GetType();
+        if (type != other.GetType()) return null; // type mis-match
+        BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+        PropertyInfo[] pinfos = type.GetProperties(flags);
+        foreach (var pinfo in pinfos)
+        {
+            if (pinfo.CanWrite)
+            {
+                try
+                {
+                    pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+                }
+                catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+            }
+        }
+        FieldInfo[] finfos = type.GetFields(flags);
+        foreach (var finfo in finfos)
+        {
+            finfo.SetValue(comp, finfo.GetValue(other));
+        }
+        return comp as T;
+    }
+
+
+}
