6c7c81bb569d9f1062c8a5ec2cd36eed4ae5e3ba
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-26 08:11:17 -0300

    created new level and minor fix on scale collision effect

--- OktaPong/Assets/Scripts/Projectile/CollideEffects/ScaleCollisionEffect.cs
+++ OktaPong/Assets/Scripts/Projectile/CollideEffects/ScaleCollisionEffect.cs
@@ -8,8 +8,15 @@ public class ScaleCollisionEffect : ProjectileCollisionEffect
     [Tooltip("Scale boost aplyed when the projectile collides")]
     [SerializeField] float scaleEffect = 0.5f;
 
+    private float currentBoos = 0f;
+
     protected override void Projectile_OnCollide()
     {
+
+        if (currentBoos >= 5f)
+            return;
+
         myProjectile.transform.localScale += new Vector3(scaleEffect, scaleEffect, scaleEffect);
+        currentBoos += scaleEffect;
     }
 }
